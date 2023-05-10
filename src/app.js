5af374db89fcbb2f2507d6381016542a3bb12b91
granacik320 <83468012+granacik320@users.noreply.github.com>
2023-05-10 22:48:51 +0200

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -11,16 +11,41 @@ const PORT = process.env.PORT || 3500;
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.use(express.static(path.join(__dirname, '../', 'public'), { extensions: ['html'] }));
 app.use('/public', express.static(path.join(__dirname, '../', 'public')));
 app.use('/CSS', express.static(path.join(__dirname, '../', 'public/CSS')));
+app.use('/images', express.static(path.join(__dirname, 'files')))
 
-app.get("/images", (req, res) => {
-    res.sendFile(path.join(__dirname, "files", "all.jpg"));
-    fs.readdirSync(path.join(__dirname, "files")).forEach(file => {
-        console.log(file);
-    });
+app.get("/images", async (req, res) => {
+    let images = getImagesFromDir(path.join(__dirname, 'files'));
+    res.render('images', { title: 'ALL IMAGES', images: images })
 });
+
+function getImagesFromDir(dirPath) {
+
+    // All iamges holder, defalut value is empty
+    let allImages = [];
+
+    // Iterator over the directory
+    let files = fs.readdirSync(dirPath);
+
+    // Iterator over the files and push jpg and png images to allImages array.
+    for (file of files) {
+        let fileLocation = path.join(dirPath, file);
+        var stat = fs.statSync(fileLocation);
+        if (stat && stat.isDirectory()) {
+            getImagesFromDir(fileLocation); // process sub directories
+        } else if (stat && stat.isFile() && ['.jpg', '.png'].indexOf(path.extname(fileLocation)) != -1) {
+            allImages.push(file); // push all .jpf and .png files to all images 
+        }
+    }
+
+    // return all images in array formate
+    return allImages;
+}
+
 app.post('/upload',
     fileUpload({ createParentPath: true }),
     filesPayloadExists,
